3beacb515955121d1f7c341d00da23a6bdcf911d
mikohan <angara99@gmail.com>
2020-10-10 19:17:30 +0300

    Added icecram feature

--- src/components/HookCakeContainer.js
+++ src/components/HookCakeContainer.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { buyCake } from '../redux';
 
 function HookCakeContainer(props) {
   const numOfCakes = useSelector((state) => state.numOfCakes);
+  const dispatch = useDispatch();
   return (
     <div>
       <h2>Number of Cakes - {numOfCakes}</h2>
-      <button>Bye cake</button>
+      <button onClick={() => dispatch(buyCake())}>Bye cake</button>
     </div>
   );
 }
---
+++ src/redux/iceCream/iceCreamActions.js
@@ -0,0 +1,7 @@
+import { BUY_ICECREAM } from './iceCreamTypes';
+
+export const byIceCream = () => {
+  return {
+    type: BUY_ICECREAM,
+  };
+};
